81121fcdf23004d54ad91b838e3bc1119c7eaee9
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2017-03-26 12:42:16 -0500

    Cambios de Estilo en el MSitio

--- resources/views/detailsArticle.blade.php
+++ resources/views/detailsArticle.blade.php
@@ -9,12 +9,41 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <div class="media">
-                            <div class="media-left">
+                            <div class="media-left" style="padding-top: 7px;">
                                 <i class="fa fa-newspaper-o fa-3x" aria-hidden="true"></i>
                             </div>
                             <div class="media-body" style="border-left: #2c3e50 solid 2px; padding-left: 7px;">
-                                <h4 class="media-heading">{{ $article->title }} <span class="label label-default">{{ $article->institution_ref }}</span> <span class="label label-default">{{ $article->grade_ref }}</span> <span class="label label-default">{{ $article->category_ref }}</span></h4>
-                                Article by {{ $article->user->first_name }} <span class="label label-default">{{ $article->user->institution_ref }}</span> <span class="label label-default">{{ $article->user->grade }}</span>
+                                
+                                <h3 class="media-heading">{{ $article->title }} 
+                                    <small> | 
+                                        <a href="{{ route('searchArticlesByInstitution', ['institution' => $article->institution_ref]) }}">{{ $article->institution_ref }}</a>, 
+
+                                        <a href="{{ route('searchArticlesByGrade', ['grade' => $article->grade_ref]) }}">{{ $article->grade_ref }}</a>, 
+                                        
+                                        <a href="{{ route('searchArticlesByCategory', ['category' => $article->category_ref]) }}">{{ $article->category_ref }}</a>
+
+                                        @if(Auth::check())
+                                            @if($article->user->id === Auth::user()->id or Auth::user()->role == "ADMIN")
+                                                @if($article->active == true)
+                                                    <span class="label label-xs label-success">Active</span>
+                                                @else
+                                                    <span class="label label-xs label-danger">Inactive</span>
+                                                @endif
+                                            @endif
+                                        @endif
+
+                                    </small>
+                                </h3>
+
+                                <h4 class="media-heading">created by <a href="{{ route('searchArticlesByUser', ['user' => $article->user->id]) }}">{{ $article->user->first_name }}</a> 
+                                    <small> | 
+                                        <a href="{{ route('searchArticlesByInstitution', ['institution' => $article->user->institution_ref]) }}">{{ $article->user->institution_ref }}</a>, 
+
+                                        <a href="{{ route('searchArticlesByGrade', ['grade' => $article->user->grade]) }}">{{ $article->user->grade }}</a>
+
+                                    </small>
+                                </h4>
+                            
                             </div>
                             @if(Auth::check())
                                 @if($article->user->id === Auth::user()->id or Auth::user()->role == "ADMIN")
@@ -28,15 +57,22 @@
 
                     <div class="panel-body">
                         <div class="row">
-                            @if($article->active == true)
-                                @include('search.articleDetailsBody')
-                            @else
-                                @if(Auth::check())
-                                    @if($article->user->id === Auth::user()->id or Auth::user()->role == "ADMIN")
-                                        @include('search.articleDetailsBody')
-                                    @endif
-                                @endif
-                            @endif
+                            <div class="col-md-12">
+                                <div class="thumbnail">
+                                    <a target="_blank" href="{{ ($article->url_video == '' ? '#!' : $article->url_video) }}">
+                                        @if($article->image == null)
+                                            <img src="/img/image404.png" alt="Image 404">
+                                        @else
+                                            <img src="/imagesArticles/{{ $article->image }}" alt="{{ $article->title }}">
+                                        @endif
+                                    </a>
+                                    <div class="caption">
+                                        <hr>
+                                        <p>{!! $article->description !!}</p>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
